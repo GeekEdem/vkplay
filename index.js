4353428118e54f5ca9d433d2cb5b788b689ad4d4
Yuri Chikhrai <4ixpar@gmail.com>
2016-10-07 20:59:22 +0000

    Initial

---
+++ index.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron')
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow()
+  win.fullscreen(true)
+
+  // and load the index.html of the app.
+  win.loadURL(`file://${__dirname}/index.html`)
+
+  // Open the DevTools.
+  win.webContents.openDevTools()
+
+  // Emitted when the window is closed.
+  win.on('closed', () => {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    win = null
+  })
+}
+
+app.on('ready', createWindow)
