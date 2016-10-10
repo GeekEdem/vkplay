99f7468a7d40aa32a69b4c0f2e717e06c2160e43
Y.Chikhray <y.chihray@gmail.com>
2016-10-10 13:38:44 +0300

    Added check credentials in db

--- index.js
+++ index.js
@@ -3,47 +3,25 @@ const authenticateVK = require('electron-vk-oauth2');
 const config = require('./config.js');
 const Datastore = require('nedb');
 var db = new Datastore({ filename: './' + config.db, autoload: true});
-const fileExists = require('file-exists');
-const fs = require('fs');
 
 function createWindow () {
     db.find({}, function (err, docs) {
-        console.log("Docs: " + docs);
         if(docs.length == 0) {
-            authentication();
+            authentication(runMainWindow);
         } else {
             var now = new Date();
-            console.log(docs[0].expires);
             if (docs[0].expires < now)
-                authentication();
+                authentication(runMainWindow);
             else{
-                console.log(docs);
+                runMainWindow(docs[0]);
             }
         }
     });
-
-    // Create the browser window.
-    // win = new BrowserWindow({
-    //   fullscreen: true,
-    //   darkTheme: true
-    // })
-    //
-    // // and load the index.html of the app.
-    // win.loadURL(`file://${__dirname}/index.html`)
-    // win.setMenu(null)
-
-    // Emitted when the window is closed.
-    // win.on('closed', () => {
-    //     // Dereference the window object, usually you would store windows
-    //     // in an array if your app supports multi windows, this is the time
-    //     // when you should delete the corresponding element.
-    //     win = null
-    // })
 }
 
 app.on('ready', createWindow);
 
-function authentication() {
+function authentication(cb) {
     authenticateVK({
         appId: config.vk.AppID,
         scope: 'audio',
@@ -54,13 +32,15 @@ function authentication() {
         console.log('User id: %s', res.userId);
         console.log('Expires in: %s', res.expiresIn);
         var now = new Date();
-        db.insert({
+        var obj = {
             accessToken: res.accessToken,
             userId: res.userId,
             expires: now.setSeconds(now.getSeconds() + res.expiresIn)
-        }, function (err) {
+        };
+        db.insert(obj, function (err) {
             // TODO: show error window!
             if (err) console.log("Save error!");
+            else cb(obj);
         });
     }).catch(function (err) {
         // E.g., user denied permissions, or user closed the window without
@@ -68,4 +48,16 @@ function authentication() {
         // TODO: show error window!
         console.error("Catch: " + err);
     });
+}
+
+function runMainWindow(credentials) {
+    win = new BrowserWindow({
+      fullscreen: true,
+      darkTheme: true
+    });
+    win.loadURL(`file://${__dirname}/index.html`);
+    win.setMenu(null);
+    win.on('closed', function() {
+        win = null;
+    })
 }
